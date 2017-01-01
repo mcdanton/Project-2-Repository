d0a260e61863cd2eb5e1f606893751117ec5b148
mcdanton <danhefter@gmail.com>
2017-01-01 18:15:24 -0500

    Working Project

--- Project2 - DanHefter/Project2 - DanHefter/TableViewCell.swift
+++ Project2 - DanHefter/Project2 - DanHefter/TableViewCell.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+
+protocol CustomCellUpdater {
+   func updateTableView()
+}
+
 class TableViewCell: UITableViewCell {
    
    
@@ -20,17 +25,18 @@ class TableViewCell: UITableViewCell {
       }
    }
    
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+   
+   override func awakeFromNib() {
+      super.awakeFromNib()
+      // Initialization code
+   }
+   
+   override func setSelected(_ selected: Bool, animated: Bool) {
+      super.setSelected(selected, animated: animated)
+      
+      // Configure the view for the selected state
+   }
+   
    
    
    func updateUI() {
@@ -38,33 +44,92 @@ class TableViewCell: UITableViewCell {
       cellLabel.text = article.title
       
       // TODO check article.image before fetch
+      
       convertImageLinkToData(imageLink: article.imageLink)
    }
    
    
    func convertImageLinkToData(imageLink: String) {
       
-      let urlRequest = URLRequest(url: URL(string: imageLink)!)
-      let urlSession = URLSession(configuration: URLSessionConfiguration.default)
-      let task = urlSession.dataTask(with: urlRequest) { (data, response, error) in
-         
-         guard let responseData = data else {
-            print("Error \(error.debugDescription): did not receive data")
-            return
+      if imageLink == "" {
+         let urlRequest = URLRequest(url: URL(string: "https://subtitling.com/scrn-content/plugins/ajax-search-pro/img/default.jpg")!)
+         let urlSession = URLSession(configuration: URLSessionConfiguration.default)
+         let task = urlSession.dataTask(with: urlRequest) { (data, response, error) in
+            
+            guard let responseData = data else {
+               print("Error \(error.debugDescription): did not receive data")
+               return
+            }
+            DispatchQueue.main.async {
+               let image = UIImage(data: responseData)
+               self.article.image = image
+               self.cellImage.image = image
+            }
          }
-         DispatchQueue.main.async {
-            print(UIImage(data: responseData))
-            let image = UIImage(data: responseData)
-            self.article.image = image
-            self.cellImage.image = image
+         task.resume()
+      } else {
+         let urlRequest = URLRequest(url: URL(string: imageLink)!)
+         let urlSession = URLSession(configuration: URLSessionConfiguration.default)
+         let task = urlSession.dataTask(with: urlRequest) { (data, response, error) in
+            
+            guard let responseData = data else {
+               print("Error \(error.debugDescription): did not receive data")
+               return
+            }
+            DispatchQueue.main.async {
+               let image = UIImage(data: responseData)
+               self.article.image = image
+               self.cellImage.image = image
+            }
          }
+         task.resume()
       }
-      task.resume()
-   }
-   
-   
-   
-   
-   
-   
 }
+
+
+}
+
+/*
+ func convertImageLinkToData(imageURL: @escaping ([Data]) -> ()) {
+ for image in Article.articles {
+ if image.imageLink == "" {
+ let urlRequest = URLRequest(url: URL(string: "http://subtitling.com/scrn-content/plugins/ajax-search-pro/img/default.jpg")!)
+ let urlSession = URLSession(configuration: URLSessionConfiguration.default)
+ let task = urlSession.dataTask(with: urlRequest) { (data, response, error) in
+ 
+ guard let responseData = data else {
+ print("Error \(error.debugDescription): did not receive data")
+ return
+ }
+ self.dataArray.append(responseData)
+ DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async {
+ imageURL(self.dataArray)
+ }
+ }
+ task.resume()
+ } else {
+ let urlRequest = URLRequest(url: URL(string: image.imageLink)!)
+ let urlSession = URLSession(configuration: URLSessionConfiguration.default)
+ let task = urlSession.dataTask(with: urlRequest) { (data, response, error) in
+ 
+ guard let responseData = data else {
+ print("Error \(error.debugDescription): did not receive data")
+ return
+ }
+ self.dataArray.append(responseData)
+ DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async {
+ imageURL(self.dataArray)
+ }
+ }
+ task.resume()
+ }
+ }
+ DispatchQueue.main.async {
+ self.myTableView.reloadData()
+ }
+ }
+ */
+
+
+
+
